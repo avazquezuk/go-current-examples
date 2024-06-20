d12450b105d6b1531a769eb460f185d1ebd7558f
avazquezuk <alejandrova@lsretail.com>
2024-06-20 09:44:51 +0200

    Latest Release for GB

---
+++ LSCentral/Scripts/Install-LatestRelease gb.ps1
@@ -0,0 +1,28 @@
+﻿#requires -RunAsAdministrator
+<#
+    .SYNOPSIS
+        Install the latest LS Central version.
+#>
+$ErrorActionPreference = 'stop'
+
+$Arguments = @{
+    'bc-server' = @{
+        AllowSessionCallSuspendWhenWriteTransactionStarted = 'true'
+    }
+}
+
+$Packages = @(
+    # Optional, uncomment to include:
+    #@{ Id = 'sql-server-express'; VersionQuery = '^-'}
+    @{ Id = 'bc-demo-database-gb'; Version = '24.0.18037.0' }
+    @{ Id = 'bc-web-client'; Version = '24.0.18037.0' }
+    @{ Id = 'bc-system-application'; Version = '24.0.16410.18056' }
+    @{ Id = 'bc-base-application-gb'; Version = '24.0.16410.18056' }
+    #@{ Id = 'ls-central-app'; Version = '' }
+    @{ Id = 'internal/ls-central-dev-license'; Version = '' }
+    #@{ Id = 'map/ls-central-to-bc'; Version = '' }
+
+    
+)
+ 
+$Packages | Install-UscPackage -InstanceName 'CostaCoffeeDev' -Arguments $Arguments
